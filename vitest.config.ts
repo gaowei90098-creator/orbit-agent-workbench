0c59392aad140fca008761077a789d4322ddb8ea
gaowei90098-creator <gaowei90098@gmail.com>
2026-06-21 01:19:35 +0800

    Use Electron stub in CI tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      electron: fileURLToPath(new URL('./test/electron-stub.ts', import.meta.url))
+    }
+  },
   test: {
     exclude: [
       '**/node_modules/**',
